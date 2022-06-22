c221639d02ea680598fd4181a2c9c2c7e9b82e2b
luciebottin <lucie.bottin@efrei.net>
2022-06-22 10:43:49 +0200

    ajout de sécurité

--- server.js
+++ server.js
@@ -151,12 +151,8 @@ app.get("/users/profile", checkAdministrator, (req, res) => {
   var query = "SELECT id, name, email, level from users"
   pool.query(query, (err, response) => {
     if (err) {
-      console.log('here');
       throw err;
     } else {
-      const users = response.rows;
-      console.log(users);
-      console.log(req.user);
       res.render("profile", { name: req.user.name, email: req.user.email });
     }
   })
@@ -164,38 +160,26 @@ app.get("/users/profile", checkAdministrator, (req, res) => {
 
 app.post("/users/profile", async (req, res) => {
   let { email, username, password, password2 } = req.body;
-
+  let name = req.user.name;
   let errors = [];
   if (password != password2) {
     errors.push({ message: "Passwords do not match" });
   }
-  console.log(password, password2, username)
   if (errors.length > 0) {
-    res.render("profile", { errors });
-  } else {
+    res.render("profile", { name, email, errors });
+  } 
+  else {
     hashedPassword = await bcrypt.hash(password, 10);
-    // Form validation has passed
+    let query = "update users set name='" + username + "', password='" + hashedPassword + "' where email='" + email + "'";
     pool.query(
-      `select * from users where email = $1`,
-      [email],
-      (err, response) => {
-        if (err) {
-          throw err;
-        }
-        else {
-          let query = "update users set name='" + username + "', password='" + hashedPassword + "' where email='" + email + "'";
-          pool.query(
-            query,
-            (err, response) => {
-              if (err) {
-                throw err;
-              }
-              res.redirect("/users/login");
-            }
-          );
-        }
-      }
-    );
+    query,
+    (err, response) => {
+    if (err) {
+      throw err;
+    }
+    req.flash("sucess_msg", "Modifications saved !");
+    res.redirect("/users/profile");
+    });
   }
 });
 
